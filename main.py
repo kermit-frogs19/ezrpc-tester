55c9d721d1169fe652354f49679dc08e6cf4b920
kermit-frogs19 <dadandboom@gmail.com>
2025-05-29 04:53:59 +0300

    chore: grpc and logs turned off

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import ping_pb2
 import ping_pb2_grpc
 
 
-try:
-    import uvloop
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-    print("system: Using uvloop for asyncio event loop")
-except ImportError:
-    uvloop = None
-    print("system: Failed to import/connect uvloop for asyncio event loop")
+# try:
+#     import uvloop
+#     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+#     print("system: Using uvloop for asyncio event loop")
+# except ImportError:
+#     uvloop = None
+#     print("system: Failed to import/connect uvloop for asyncio event loop")
 
 
 async def main():
@@ -33,26 +33,26 @@ async def main():
     # initial test
     for i in range(batches_amount):
         print(f"\n\nStarting batch #{i+1}")
-        channel = grpc.insecure_channel("nyc3.seliukov.com:50051")
-        stub = ping_pb2_grpc.PingerStub(channel)
-        # client = Producer("https://nyc3.seliukov.com:8000", use_tls=False, timeout=None)
+        # channel = grpc.insecure_channel("nyc3.seliukov.com:50051")
+        # stub = ping_pb2_grpc.PingerStub(channel)
+        client = Producer("https://nyc3.seliukov.com:8000", use_tls=False, timeout=None)
 
         for _ in range(5):
-            stub.Ping(ping_pb2.Empty())
-            # await client.ping()
+            # stub.Ping(ping_pb2.Empty())
+            await client.ping()
 
         # here the timer start
         start = time.perf_counter()
         for _ in range(batch_size):
-            stub.Ping(ping_pb2.Empty())
-            # await client.ping()
+            # stub.Ping(ping_pb2.Empty())
+            await client.ping()
         end = time.perf_counter()
         duration = end - start
         durations.append(duration)
         print(f"Batch {i+1}: {batch_size} requests took {duration:.6f} seconds. Average time per request: {duration / batch_size:.6f} seconds")     # here the timer end
 
-        channel.close()
-        # await client.close()
+        # channel.close()
+        await client.close()
 
     print(f"\n[SUMMARY] {batches_amount} batches × {batch_size} requests")
     print(f"\n[SUMMARY] Collected results: {durations}")
